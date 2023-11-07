d5e33a058f5d2eaca846659b34d1c9f78405ed09
ikhlasdansantai <initesting5@gmail.com>
2023-11-07 19:00:06 +0700

    Menambahkan page blog

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import BlogCard from "../components/common/Fragments/BlogCard";
+
+export default function page() {
+  return (
+    <div className="py-10 sm:pt-20 lg:pt-40 px-4 space-y-10 max-w-6xl mx-auto min-[2000px]:h-[40rem]">
+      <div className="titles mb-10">
+        <h1 className="text-white relative overflow-hidden">
+          <span className="text-white/[.2] text-[6.25rem] font-bold leading-[normal] opacity-25">Blogs</span>
+          <b className="absolute left-20 bottom-0 top-8 mx-auto text-white text-[2.5rem] font-bold">Blogs</b>
+        </h1>
+      </div>
+      <BlogCard />
+    </div>
+  );
+}
