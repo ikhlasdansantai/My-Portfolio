a6a66d1d8dd9f063bba6f8f630ed903bcfbf33ef
ikhlasdansantai <initesting5@gmail.com>
2023-11-09 09:15:33 +0700

    menghapus deadcode

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -92,12 +92,3 @@ export default function Page() {
     </>
   );
 }
-
-{
-  /* <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
-<g opacity="0.6">
-<circle cx="8.5" cy="8.5" r="8.5" transform="rotate(180 8.5 8.5)" fill="white" fill-opacity="0.5"/>
-<circle cx="8.5" cy="8.5" r="4.5" transform="rotate(180 8.5 8.5)" fill="white"/>
-</g>
-</svg> */
-}
