19997c635a6d88808b11b810a6dd682a08da9161
ikhlasdansantai <initesting5@gmail.com>
2023-11-10 20:25:37 +0700

    menambahkan component untuk handle project card

--- src/app/components/common/Fragments/ProjectCard.tsx
+++ src/app/components/common/Fragments/ProjectCard.tsx
@@ -1,31 +1,32 @@
-export default function ProjectCategory({ category }: { category: any }) {
+import Image from "next/image";
+
+export default function ProjectCard({ show, setShow, category }: any) {
   if (category === "semua") {
-    return (
-      <div className="text-white grid grid-cols-2 gap-10 ">
-        <div className="card h-[200px] bg-red-500"></div>
-        <div className="card h-[200px] bg-red-500"></div>
-      </div>
-    );
+    return <h2>Semua kategori</h2>;
   } else if (category === "ui design") {
-    return (
-      <div className="text-white grid grid-cols-2 gap-10 ">
-        <div className="card h-[200px] bg-green-500"></div>
-        <div className="card h-[200px] bg-green-500"></div>
-      </div>
-    );
+    return <h2>Semua kategori</h2>;
   } else if (category === "web development") {
-    return (
-      <div className="text-white grid grid-cols-2 gap-10 ">
-        <div className="card h-[200px] bg-orange-500"></div>
-        <div className="card h-[200px] bg-orange-500"></div>
-      </div>
-    );
+    return <h2>Semua kategori</h2>;
   } else if (category === "motion graphics") {
-    return (
-      <div className="text-white grid grid-cols-2 gap-10 ">
-        <div className="card h-[200px] bg-white"></div>
-        <div className="card h-[200px] bg-white"></div>
-      </div>
-    );
+    return <h2>Semua kategori</h2>;
   }
+  return (
+    <div className="text-white grid md:grid-cols-2 gap-10 ">
+      <div onClick={() => setShow(true)} className="pt-4 px-4 pb-6 bg-white/[.03] rounded-lg ">
+        <figure className="rounded-md overflow-hidden">
+          <Image src={"https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/100DaysOfCode.png"} alt="gambar" height="200" width="200" style={{ width: "100%", height: "auto" }} loading="lazy" quality={100} />
+        </figure>
+        <h2 className="text-white mt-4 mb-1">100 Days Of Code</h2>
+        <span className="text-white/[.60] text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis repellat libero esse iure, fugit magni.</span>
+      </div>
+
+      <div onClick={() => setShow(true)} className="pt-4 px-4 pb-6 bg-white/[.03] rounded-lg ">
+        <figure className="rounded-md overflow-hidden">
+          <Image src={"https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/Hokben%20Clone.png"} alt="gambar" height="200" width="200" style={{ width: "100%", height: "auto" }} loading="lazy" quality={100} />
+        </figure>
+        <h2 className="text-white mt-4 mb-1">Hokben Clone</h2>
+        <span className="text-white/[.60] text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis repellat libero esse iure, fugit magni.</span>
+      </div>
+    </div>
+  );
 }
