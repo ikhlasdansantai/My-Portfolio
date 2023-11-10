58b91ae57ab2b77061f7d98562b406ff6465d426
ikhlasdansantai <initesting5@gmail.com>
2023-11-10 20:27:40 +0700

    menambahkan comp untuk handle techstack

--- src/app/components/common/Fragments/TechListCard.tsx
+++ src/app/components/common/Fragments/TechListCard.tsx
@@ -5,7 +5,7 @@ export default function TechListCard({ name, url, status }: { name: string; url:
   return (
     <div className={`${styles.tech} tech group flex gap-3 justify-start items-center text-white border px-4 py-3 cursor-pointer`}>
       <figure className={`${styles["tech__pict"]} text__pict w-8 transition-all`}>
-        <Image src={url} alt={`${url} Image`} width={0} height={0} style={{ width: "100%", height: "auto" }} quality={100} loading="lazy" />
+        <Image src={url} alt={`${url} Image`} width={40} height={40} quality={100} loading="lazy" />
       </figure>
       <div className={`${styles["tech__info"]} relative  w-full`}>
         <h5 className="relative top-3 bottom-0 transition-all cut__text">{name}</h5>
