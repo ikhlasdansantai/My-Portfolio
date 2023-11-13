abec7f4b7ebc70b0a9973ab111f5f429c78de9ee
ikhlasdansantai <initesting5@gmail.com>
2023-11-13 20:01:20 +0700

    memperbaiki dan mengimprove fitur di comp project card beserta zustand projects

--- src/app/components/common/Fragments/ProjectCard.tsx
+++ src/app/components/common/Fragments/ProjectCard.tsx
@@ -1,31 +1,97 @@
-export default function ProjectCategory({ category }: { category: any }) {
-  if (category === "semua") {
+"use client";
+
+import Image from "next/image";
+import { useProjectStore } from "@/app/store/projectCategoryStore";
+import { useShallow } from "zustand/react/shallow";
+import classNames from "classnames";
+
+interface Projects {
+  name: string;
+  url: string;
+  description: string;
+  category: string;
+  links: any;
+  technology: string[];
+}
+
+interface ProjectCardProps {
+  projects: Projects[];
+}
+
+export default function ProjectCard({ projects }: ProjectCardProps) {
+  const { category, setProjDetail, setProjectDetail } = useProjectStore(
+    useShallow((state) => ({
+      category: state.projCateFilters,
+      setProjDetail: state.setShowProjDetail,
+      setProjectDetail: state.setProjectDetail,
+    }))
+  );
+
+  const projFilter = projects.map((project) => project).filter((project) => project.category === category);
+
+  if (category === "semua kategori") {
     return (
-      <div className="text-white grid grid-cols-2 gap-10 ">
-        <div className="card h-[200px] bg-red-500"></div>
-        <div className="card h-[200px] bg-red-500"></div>
-      </div>
-    );
-  } else if (category === "ui design") {
-    return (
-      <div className="text-white grid grid-cols-2 gap-10 ">
-        <div className="card h-[200px] bg-green-500"></div>
-        <div className="card h-[200px] bg-green-500"></div>
-      </div>
-    );
-  } else if (category === "web development") {
-    return (
-      <div className="text-white grid grid-cols-2 gap-10 ">
-        <div className="card h-[200px] bg-orange-500"></div>
-        <div className="card h-[200px] bg-orange-500"></div>
-      </div>
-    );
-  } else if (category === "motion graphics") {
-    return (
-      <div className="text-white grid grid-cols-2 gap-10 ">
-        <div className="card h-[200px] bg-white"></div>
-        <div className="card h-[200px] bg-white"></div>
+      <div className="text-white grid sm:grid-cols-2 gap-10 ">
+        {projects.map(({ name, url, description, category, links, technology }, index): any => (
+          <div
+            key={index}
+            onClick={() => {
+              setProjectDetail({ name, url, description, category, links, technology });
+              setProjDetail(true);
+            }}
+            className="pt-4 px-4 pb-6 bg-white/[.03] rounded-lg "
+          >
+            <figure className="rounded-md overflow-hidden">
+              <Image
+                src={url}
+                alt="gambar"
+                height="200"
+                width="200"
+                style={{ width: "100%", height: "auto" }}
+                className={classNames({
+                  "blur-[7px]": category === "wip",
+                })}
+                loading="lazy"
+                quality={100}
+              />
+            </figure>
+            <h2 className="text-white mt-4 mb-1">{name}</h2>
+            <span className="text-white/[.60] text-xs">{description}.</span>
+          </div>
+        ))}
       </div>
     );
   }
+
+  return (
+    <div className="text-white grid md:grid-cols-2 gap-10 ">
+      {projFilter.map(({ name, url, description, category, links, technology }, index): any => (
+        <div
+          key={index}
+          onClick={() => {
+            setProjectDetail({ name, url, description, category, links, technology });
+            setProjDetail(true);
+          }}
+          className="pt-4 px-4 pb-6 bg-white/[.03] rounded-lg "
+        >
+          <figure className="rounded-md overflow-hidden">
+            <Image
+              src={url}
+              alt="gambar"
+              height="200"
+              width="200"
+              style={{ width: "100%", height: "auto" }}
+              loading="lazy"
+              className={classNames({
+                "blur-[7px]": category === "wip",
+              })}
+              quality={100}
+            />
+          </figure>
+          <h2 className="text-white mt-4 mb-1">{name}</h2>
+          <span className="text-white/[.60] text-xs">{description}.</span>
+        </div>
+      ))}
+    </div>
+  );
 }
