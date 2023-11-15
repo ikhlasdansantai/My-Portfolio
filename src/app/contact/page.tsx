5b7116e1f378253b7d5778586afc8eb9d29b1491
ikhlasdansantai <initesting5@gmail.com>
2023-11-15 20:27:29 +0700

    ternyata classNames tidak perlu client components :3

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,47 +1,12 @@
-"use client";
 import type { Metadata } from "next";
 import classNames from "classnames";
+import ContactReady from "../components/containers/contactReady";
+import ContactNotReady from "../components/containers/ContactNotReady";
 
 export const metadata: Metadata = {
   title: "Contact",
   description: "Work In Progress:3",
 };
-
-function ContactNotReady() {
-  return <h1 className="text-2xl text-white text-center mx-auto">Masih Dalam Pembangunan Yagesya</h1>;
-}
-
-function ContactReady() {
-  return (
-    <>
-      <div className="titles">
-        <h1 className="text-white font-bold text-4xl">Let's make something amazing together</h1>
-        <p className="text-white/[.60] mt-2">
-          Dimulai dengan bilang <span className="font-semibold">Hallo!</span>
-        </p>
-      </div>
-      <form action="" className="space-y-4">
-        <div className="boxInput">
-          <input type="text" id="username-success" className="bg-green-50 border placeholder-green-700 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 " placeholder="Nama" />
-        </div>
-        <div className="boxInput">
-          <input type="email" id="username-success" className="bg-green-50 border placeholder-green-700 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 " placeholder="Email" />
-        </div>
-        <div className="boxInput">
-          <input type="text" id="username-success" className="bg-green-50 border placeholder-green-700 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 " placeholder="Tipe Project" />
-        </div>
-        <div className="boxInput">
-          <textarea
-            id="message"
-            rows={5}
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Leave a comment..."
-          ></textarea>
-        </div>
-      </form>
-    </>
-  );
-}
 
 export default function Page() {
   const contactPageReady = false;
