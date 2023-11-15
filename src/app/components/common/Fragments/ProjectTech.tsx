4efb6adf9f7ea501dfda8afa1d3192029b9ae911
ikhlasdansantai <initesting5@gmail.com>
2023-11-15 12:08:43 +0700

    refactor/fix code for projects page

--- src/app/components/common/Fragments/ProjectTech.tsx
+++ src/app/components/common/Fragments/ProjectTech.tsx
@@ -1,45 +1,13 @@
 import Image from "next/image";
 
-export default function ProjectTech() {
+export default function ProjectTech({ technology }: any) {
   return (
     <div className="project__tech__used flex flex-wrap gap-3">
-      <figure className="w-7">
-        <Image src={"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"} alt="gambar" height={100} width={400} style={{ width: "100%", height: "auto" }} loading="lazy" quality={100} />
-      </figure>
-      <figure className="w-7">
-        <Image
-          src={"https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/ExpressJS-Dark.svg"}
-          alt="gambar"
-          height={100}
-          width={400}
-          style={{ width: "100%", height: "auto" }}
-          loading="lazy"
-          quality={100}
-        />
-      </figure>
-      <figure className="w-7">
-        <Image src={"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"} alt="gambar" height={100} width={400} style={{ width: "100%", height: "auto" }} loading="lazy" quality={100} />
-      </figure>
-      <figure className="w-7">
-        <Image src={"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"} alt="gambar" height={100} width={400} style={{ width: "100%", height: "auto" }} loading="lazy" quality={100} />
-      </figure>
-      <figure className="w-7">
-        <Image src={"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"} alt="gambar" height={100} width={400} style={{ width: "100%", height: "auto" }} loading="lazy" quality={100} />
-      </figure>
-      <figure className="w-7">
-        <Image
-          src={"https://raw.githubusercontent.com/tandpfun/skill-icons/59059d9d1a2c092696dc66e00931cc1181a4ce1f/icons/NextJS-Dark.svg"}
-          alt="gambar"
-          height={100}
-          width={400}
-          style={{ width: "100%", height: "auto" }}
-          loading="lazy"
-          quality={100}
-        />
-      </figure>
-      <figure className="w-7">
-        <Image src={"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg"} alt="gambar" height={100} width={400} style={{ width: "100%", height: "auto" }} loading="lazy" quality={100} />
-      </figure>
+      {technology.map((tech, index) => (
+        <figure key={index} className="w-7">
+          <Image src={tech} alt="gambar teknologi" height={100} width={400} style={{ width: "100%", height: "auto" }} loading="lazy" quality={100} />
+        </figure>
+      ))}
     </div>
   );
 }
--- src/app/components/layouts/Projects/ProjectMenus.tsx
+++ src/app/components/layouts/Projects/ProjectMenus.tsx
@@ -1,14 +1,9 @@
-"use client";
-
 import ProjectMenuCategories from "@/app/components/common/Fragments/ProjectMenuCategories";
-import { useEffect, useState } from "react";
 
 export default function ProjectMenus() {
-  const [category, setCategory] = useState("semua kategori");
-
   return (
-    <div className="project__menus mt-10 space-y-4">
-      <ProjectMenuCategories category={category} setCategory={setCategory} />
+    <div className="mt-10 space-y-4 relative z-20">
+      <ProjectMenuCategories />
     </div>
   );
 }
