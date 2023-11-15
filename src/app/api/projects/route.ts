168bf51fd7840ce597a50e37630d2ecc3ce92d2f
ikhlasdansantai <initesting5@gmail.com>
2023-11-15 12:03:32 +0700

    memperbaiki deskripsi proyek untuk pages projects

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -6,7 +6,7 @@ export async function GET() {
       id: 1,
       name: "ww2 Trailer",
       url: "https://img.youtube.com/vi/tBTMDELzvVA/maxresdefault.jpg",
-      description: "Big Project dengan tema ww2 dengan konsep motion graphics, cuman sekarang lagi vakum, jadi jangan terlalu berharap project nya akan di lanjut besok/hari ini:v, jadi sabar aja, subscribe hehe",
+      description: "Big Project dengan tema ww2 dengan konsep motion graphics, cuman sekarang lagi vakum:(, jadi jangan terlalu berharap project nya akan di lanjut besok/hari ini:v, jadi sabar aja, jangan lupa subscribe hehe",
       links: {
         livePreview: "https://youtu.be/tBTMDELzvVA?si=undvhqeufxuvlbgJ",
       },
@@ -21,7 +21,7 @@ export async function GET() {
       id: 2,
       name: "BWA Slicing Challenge",
       url: "https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/Proj/Build-With-Angga-Challenge-new.png",
-      description: "Projek Challenge pertama dengan design UI dari BuildWithAngga",
+      description: "Projek Challenge pertama menggunakan Bootstrap dengan design UI dari BuildWithAngga (instagram)",
       links: {
         livePreview: "https://ikhlasdansantai.github.io/build-with-angga-slicing-challenge/",
       },
@@ -52,7 +52,7 @@ export async function GET() {
       id: 4,
       name: "Digital Web Profile",
       url: "https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/Proj/digital-web-new.png",
-      description: "Terinspirasi dari sebuah web builder populer, yaitu linktree",
+      description: "Terinspirasi dari sebuah website builder populer, yaitu linktree",
       links: {
         livePreview: "https://ikhlasdansantai.github.io/My-Digital-Website/",
       },
@@ -67,7 +67,7 @@ export async function GET() {
       id: 5,
       name: "Foodology Slicing",
       url: "https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/Proj/Foodology-new.png",
-      description: "Melakukan slicing pertama dengan design dari figma",
+      description: "Proyek slicing pertama dengan memanfaatkan penggunaan figma yang di terjemahkan ke dalam sebuah kode untuk bisa di akses oleh semua orang",
       links: {
         livePreview: "https://ikhlasdansantai.github.io/Foodology-Web/",
       },
@@ -82,7 +82,7 @@ export async function GET() {
       id: 6,
       name: "Hokben Clone",
       url: "https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/Proj/Hokben-Clone-new.png",
-      description: "Membuat web clone pertama dan melakukan redesign, terhadap design dari website Hokben",
+      description: "Membuat sebuah website clone pertama, tidak hanya itu, saya melakukan redesign terhadap design original dari website Hokben",
       links: {
         livePreview: "https://hokben-clone-by-ikhlasdansantai.netlify.app/",
       },
@@ -97,7 +97,8 @@ export async function GET() {
       id: 7,
       name: "Karang Taruna Wall Maria",
       url: "https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/Proj/Karangtaruna-new.png",
-      description: "Untuk Pertama kalinya saya menggunakan figma untuk membuat struktur website",
+      description:
+        "Terinspirasi dari suatu organisasi masyarakat, sebagai wadah generasi muda untuk mengembangkan diri, tumbuh, dan berkembang atas dasar kesadaran serta tanggung jawab sosial dari, oleh dan untuk generasi muda, yang berorientasi pada tercapainya kesejahteraan sosial masyarakat.",
       links: {
         livePreview: "https://ikhlasdansantai.github.io/karang-taruna-wall-maria/",
       },
@@ -113,7 +114,7 @@ export async function GET() {
       id: 8,
       name: "CV Rizky Steel",
       url: "https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/Rizky%20Steel.png",
-      description: "Proyek Website client pertama saya",
+      description: "Proyek Website dengan bertemakan konstruksi yang mengenalkan sebuah bisnis/jasa renovasi rumah",
       links: {
         livePreview: "https://www.figma.com/file/V2haWUswbO6bdsD0Qf4dPk/CV-Rizky-Steel?type=design&node-id=0%3A1&mode=design&t=fC7Ckz946IhivJwf-1",
       },
@@ -124,7 +125,7 @@ export async function GET() {
       id: 9,
       name: "Renz Makeup",
       url: "https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/Proj/Renz-Makeup-new.png",
-      description: "Sebuah Proyek personal branding, di dalam proyek ini saya menggunakan tailwindCSS untuk pertama kalinya",
+      description: "Sebuah Proyek personal branding, di dalam proyek ini untuk pertama kalinya saya mengimplementasikan penggunaan tailwindCSS untuk membuat sebuah website",
       links: {
         livePreview: "https://renz-makeup.netlify.app/",
       },
@@ -141,7 +142,7 @@ export async function GET() {
       id: 10,
       name: "Renz Aura",
       url: "https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/Proj/RenzAura-new.png",
-      description: "Sebuah Proyek personal branding, di dalam proyek ini saya menggunakan tailwindCSS untuk pertama kalinya",
+      description: "Sebuah proyek personal branding, di dalamnya kamu dapat berpartisipasi untuk mempromosikan barang atau jasa yang kamu miliki, sehingga harapannya dapat meningkatkan jumlah pengunjung dan pembeli",
       links: {
         livePreview: "https://ikhlasdansantai.github.io/RenzAura/",
       },
@@ -152,7 +153,8 @@ export async function GET() {
       id: 11,
       name: "Weeekly Fanmade Website",
       url: "https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/Proj/Weeekly-Fanmade-Website-new.png",
-      description: "Proyek latihan pertama saya yang menggunakan ReactJS",
+      description:
+        "Sebuah proyek yang awalnya iseng karena suka sama musiknya yang After School, sehingga saya melakukan riset untuk mencari official website dari group Weeekly ini, dan ternyata mereka belum punya website resminya, hingga akhirnya saya coba untuk bikin versi saya sendiri.",
       links: {
         livePreview: "https://ikhlasdansantai.github.io/weeekly-website-fanmade/",
       },
@@ -163,7 +165,7 @@ export async function GET() {
       id: 12,
       name: "Hangeulin",
       url: "https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/HangeulIn.png",
-      description: "Sebuah Proyek personal branding, di dalam proyek ini saya menggunakan tailwindCSS untuk pertama kalinya",
+      description: "Untuk kesekian lagi, saya mencoba untuk mengerjakan proyek slicing dengan menggunakan teknologi react + tailwindcss ",
       links: {
         livePreview: "https://ikhlasdansantai.github.io/Hangeulin/",
       },
@@ -174,7 +176,7 @@ export async function GET() {
       id: 12,
       name: "Camp Yang Paling Mengerikan Di Era ww2",
       url: "https://img.youtube.com/vi/BW9glb4e0lM/maxresdefault.jpg",
-      description: "Sebuah Proyek personal branding, di dalam proyek ini saya menggunakan tailwindCSS untuk pertama kalinya",
+      description: "Menceritakan tentang kejahatan perang yang dilakukan oleh nasi jerman di era perang dunia ke 2, di video ini saya menjelaskan tentang salah satu camp konsentrasi jerman, yaitu Dachau",
       links: {
         livePreview: "https://youtu.be/BW9glb4e0lM?si=tUt-bSSd_CgDXuF8",
       },
@@ -189,30 +191,22 @@ export async function GET() {
       id: 12,
       name: "tebak 1",
       url: "https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/Proj/ive.png",
-      description: "Sebuah Proyek personal branding, di dalam proyek ini saya menggunakan tailwindCSS untuk pertama kalinya",
+      description: "masih di rahasiakan, tapi jika kamu tau proyek apa ini, berarti anda adalah seorang fans :D",
       links: {
         livePreview: "",
       },
-      technology: [
-        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
-        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
-        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
-      ],
+      technology: [""],
       category: "wip",
     },
     {
       id: 12,
       name: "tebak 2",
       url: "https://raw.githubusercontent.com/ikhlasdansantai/My-Portfolio/main/assets/projects/Proj/isone-docs.png",
-      description: "Sebuah Proyek personal branding, di dalam proyek ini saya menggunakan tailwindCSS untuk pertama kalinya",
+      description: "masih di rahasiakan, tapi jika kamu tau proyek apa ini, berarti anda adalah seorang fans :D",
       links: {
         livePreview: "",
       },
-      technology: [
-        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
-        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
-        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
-      ],
+      technology: [""],
       category: "wip",
     },
   ];
