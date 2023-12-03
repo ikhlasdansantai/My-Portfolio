d09e6299c546c3945588bb8ee020c4f915c050bd
ikhlasdansantai <initesting5@gmail.com>
2023-12-03 22:05:22 +0700

    memperbaiki style untuk layout all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/layouts/Navbar/Navbar";
+import Footer from "./components/layouts/Footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,11 +14,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#1e1f2b]`}>
+      <body className={`${inter.className} bg-[#1e1f2b] flex min-h-screen flex-col justify-evenly`}>
         <header>
           <Navbar />
         </header>
-        {children}
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
