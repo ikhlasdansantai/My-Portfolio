d7d3395fee487a0b478680f9324554e55e9802c7
“ikhlasdansantai” <initesting5@gmail.com>
2024-05-24 10:28:46 +0700

    config(nextjs.config): change images remotePatterns config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.jsdelivr.net", "skillicons.dev", "cdn.worldvectorlogo.com", "raw.githubusercontent.com", "cdn-images-1.medium.com", "cdn.simpleicons.org", "media.giphy.com", "img.youtube.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn-images-1.medium.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "skillicons.dev",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.worldvectorlogo.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "media.giphy.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    // domains: ["cdn.jsdelivr.net", "skillicons.dev", "cdn.worldvectorlogo.com", "raw.githubusercontent.com", "cdn-images-1.medium.com", "cdn.simpleicons.org", "media.giphy.com", "img.youtube.com"],
   },
 };
 
